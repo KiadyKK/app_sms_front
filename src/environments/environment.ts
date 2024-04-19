aea0cd0f2306ad2e1e815a340b0a06ee78ef9353
RAMANJATO Kiadinirina Biaza <biaza.ramanjato@orange.com>
2024-04-19 08:48:49 +0300

    front-kiady

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  // apiUrl: 'http://localhost:8080/'
+  apiUrl: 'http://localhost:8080/'
   // apiUrl: 'https://kpiperf.intranet.oma/api/',
-  apiUrl: '/',
+  // apiUrl: '/',
 };
